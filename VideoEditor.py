6d1951ed5528b579ec2fd0cf829da50433ea33cb
unknown <drunktolstoy@gmail.com>
2022-05-14 17:58:58 +0300

    getting subs from json

--- VideoEditor.py
+++ VideoEditor.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 URL = "https://api.opensubtitles.com/api/v1/subtitles"
+DOWNLOAD_LINK_REQ = "https://api.opensubtitles.com/api/v1/download"
 headers = { 'Content-Type': 'application/json', "Api-Key": os.getenv("API_KEY") }
 
 generator = lambda txt:TextClip(txt, font='Arial', fontsize=24, color='white')
@@ -16,7 +17,10 @@ class VideoEditor:
         self.file = file
         PARAMS = {"query":file}
         r = requests.get(url=URL,params=PARAMS,headers=headers)
-        print(r.json()) 
+        srt_file = r.json()["data"][0]["attributes"]["files"][0]
+        srt_file_name = srt_file["file_name"]
+        print(f"Found subtitles for {file}: {srt_file_name}")
+        #download_link_body = { "file_id":  }
 
     """def add_subtitles(self,file:str):
         ffmpeg
